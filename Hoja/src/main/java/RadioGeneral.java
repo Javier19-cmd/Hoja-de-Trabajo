78a5cca03ffeba9a51379091f50477e6904d31de
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-01-17 10:31:34 -0600

    Agregando los métodos de la interface

--- Hoja/src/main/java/RadioGeneral.java
+++ Hoja/src/main/java/RadioGeneral.java
@@ -10,4 +10,12 @@
  */
 public interface RadioGeneral {
     
+    boolean isOn();     //Se usará para ver si el radio está o no encendido y en caso de no estar encendido, le prohíbe al main hacer que funcione el radio.
+    void encender();    //Pone al boolean del radio en true. Esto enciende el radio.
+    void apagar();      //Pone el boolean del radio como false. Esto apagará por completo al radio.
+    void incrementar(); //Avanza la emisora. Esto verifica si la emisora es de am o fm.
+    void disminuir();   //Retrocede la emisra También verifica si la emisora es am o fm. 
+    void asignar();     //Creará una arraylist o array con las emisoras de am y fm.
+    void emisoras();    //Enseña la lista de estaciones de radio ya sea que esté en am o fm.
+    void frecuencia();  //Cambiar de am a fm o a am. 
 }
